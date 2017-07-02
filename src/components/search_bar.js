59733908ae576ba84423c808bd5bc2477217100a
dannyradden <dradden@gmail.com>
2017-07-02 00:13:39 -0600

    Add value to input in search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props)
+  constructor( props ) {
+    super( props )
 
     this.state = {
       term: ''
     }
   }
-  render() {
+  render( ) {
     return (
       <div>
-        <input onChange={event => this.setState({term: event.target.value})}/>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}/>
       </div>
     )
   }
