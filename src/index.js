e1959ca2984ea8b0153a84f9be167c13b6404f66
dannyradden <dradden@gmail.com>
2017-07-02 13:15:08 -0600

    Add YTsearch to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyClUVDem-M4VsZhjFUmhkOD91jJ7iS0fYs'
+
+
+YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+  console.log(data)
+})
 
 const App = () => {
   return (
